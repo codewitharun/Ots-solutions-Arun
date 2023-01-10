7383358399429ec2aec26a9e32993183628713fb
arun.kumar <arun.kumar@otssolutions.com>
2023-01-10 19:49:13 +0530

    7:49

--- ReactNativeTraining/App.js
+++ ReactNativeTraining/App.js
@@ -1,82 +1,15 @@
-import {SafeAreaView, StyleSheet, Text, View, Image} from 'react-native';
-import React, {useState, useEffect} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Products from './src/Screens/products';
-import Rating from './assests/rating/star-outline.svg';
-function DetailsScreen(props) {
-  const [items, setItems] = useState();
-  useEffect(() => {
-    setItems(props?.route?.params?.product);
-    // console.log(props?.route?.params?.product);
-  }, []);
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Products from './src/Screens/Products';
 
+const App = ({navigation}) => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <View
-        style={{
-          // backgroundColor: 'green',
-          borderRadius: 10,
-          borderWidth: 1,
-          height: '52%',
-          width: '70%',
-        }}>
-        <Image
-          source={{uri: items?.images[0]}}
-          style={{
-            height: 200,
-            width: 200,
-            alignSelf: 'center',
-            marginTop: 10,
-            resizeMode: 'contain',
-          }}
-        />
-        <Text style={{fontSize: 20, paddingLeft: 10}}>{items?.title}</Text>
-        <Text style={{paddingLeft: 10}}>
-          <Text style={{fontWeight: 'bold', paddingLeft: 10}}>Brand:</Text>
-          {items?.brand}
-        </Text>
-        <Text style={{fontWeight: 'bold', paddingLeft: 10}}>
-          Price after Discount:
-          <Text style={{fontSize: 20, paddingLeft: 10}}> {items?.price}</Text>
-        </Text>
-        <Text style={{paddingLeft: 10}}>
-          <Text style={{fontWeight: 'bold', color: 'red', paddingLeft: 10}}>
-            Discount {items?.discountPercentage} %
-          </Text>
-        </Text>
-
-        <Text style={{fontWeight: 'bold'}}>
-          {items?.rating}
-
-          <Text style={{paddingLeft: 10}}>
-            <Rating width={20} height={20} color="orange" paddingLeft={50} />
-            User ratings
-          </Text>
-        </Text>
-        <Text style={{paddingLeft: 10}}>
-          <Text style={{fontWeight: 'bold', paddingLeft: 10}}>
-            Description:
-          </Text>
-          {items?.description}
-        </Text>
-      </View>
+    <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+      <Products keyType={'number-pad'} />
+      <Products keyType={'email-address'} />
     </View>
   );
-}
-
-const Stack = createNativeStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Products} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+};
 
 export default App;
 
--- ReactNativeTraining/src/Screens/products.js
+++ ReactNativeTraining/src/Screens/products.js
@@ -1,121 +1,26 @@
 /* eslint-disable react-native/no-inline-styles */
-import {
-  Button,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  FlatList,
-} from 'react-native';
-import React, {useState, useEffect} from 'react';
+import {StyleSheet, Text, View, TextInput, SafeAreaView} from 'react-native';
+import React from 'react';
 
-import axios, {isCancel, AxiosError} from 'axios';
-
-const Products = ({navigation}) => {
-  const [hello, setHello] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get('https://dummyjson.com/products')
-      .then(function (response) {
-        // handle success
-        // console.log();
-        setHello(response.data.products);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .finally(function () {
-        // always executed
-      });
-  }, []);
-  const handleclick = selectedProduct => {
-    navigation.navigate('Details', {
-      product: selectedProduct,
-    });
-  };
+const Products = ({navigation, keyType}) => {
   return (
-    <View
-      style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        flex: 1,
-        width: '100%',
-      }}>
-      {/* {hello &&
-          hello.map(e => {
-            return (
-              <View
-                key={e.id}
-                style={{
-                  flex: 1,
-                  // backgroundColor: 'black',
-                  width: '90%',
-                  borderColor: 'black',
-                  borderWidth: 1,
-                  padding: 20,
-                  justifyContent: 'center',
-                  // alignItems: 'center',
-                  margin: 10,
-                }}>
-                <View
-                  style={{
-                    // alignSelf: 'center',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}>
-                  <Image
-                    style={{height: 200, width: 200, resizeMode: 'contain'}}
-                    source={{
-                      uri: e?.images[0],
-                    }}
-                  />
-                  <View>
-                    <TouchableOpacity onPress={() => handleclick(e)}>
-                      <Text style={{fontSize: 24}}>{e.title}</Text>
-                    </TouchableOpacity>
-                    <Text style={{fontSize: 19}}>Mrp:{e.price}</Text>
-                  </View>
-                </View>
-              </View>
-            );
-          })} */}
-      <FlatList
-        data={hello}
-        renderItem={item => {
-          return (
-            <View
-              style={{
-                flex: 1,
-                flexWrap: 'wrap',
-                alignSelf: 'center',
-                borderWidth: 1,
-                margin: 30,
-                width: '90%',
-              }}>
-              {console.log(item.item)}
-              <TouchableOpacity onPress={() => handleclick(item.item)}>
-                <Image
-                  source={{uri: item?.item?.images[0]}}
-                  style={{
-                    height: 200,
-                    width: 200,
-                    resizeMode: 'contain',
-                  }}
-                />
-              </TouchableOpacity>
-              <Text>
-                {item?.item?.title} <Text>{item?.item?.brand}</Text>
-              </Text>
-              <Text> {item?.item?.title}</Text>
-            </View>
-          );
-        }}
-      />
-    </View>
+    <SafeAreaView>
+      <View style={{}}>
+        <Text style={{lineHeight: 50}}>KeyboardType: {keyType}</Text>
+        <TextInput
+          style={{
+            borderWidth: 1,
+            width: '90%',
+            paddingLeft: 30,
+            height: 60,
+            borderRadius: 7,
+          }}
+          keyboardType={keyType}
+          placeholder={keyType}
+          keyboardAppearance="dark"
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
