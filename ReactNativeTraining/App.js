f0800bc840dfe3564d3ad308bdb22f6660287935
codewitharun <arunk4it@gmail.com>
2023-01-12 13:36:39 +0530

    home

--- ReactNativeTraining/App.js
+++ ReactNativeTraining/App.js
@@ -1,6 +1,6 @@
 import {StatusBar, StyleSheet, Text, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
-import Products from './src/Screens/Products';
+import Products from './src/Screens/products';
 
 import TextBox from './src/Screens/Textbox';
 import Touch from './src/Screens/Touch';
